9779a5f5b6f38c7da1a06b17548d159da8d364ba
Chedlyrdissi <chedlyrd2000@gmail.com>
2020-09-10 22:16:54 -0400

    adding shotcuts

--- src/app/components/board/cell/pieces/pieces-names.enum.ts
+++ src/app/components/board/cell/pieces/pieces-names.enum.ts
@@ -6,3 +6,10 @@ export enum PieceName {
   Queen = 'queen',
   Rook = 'rook'
 }
+
+export const pawn = PieceName.Pawn;
+export const knight = PieceName.Knight;
+export const rook = PieceName.Rook;
+export const bishop = PieceName.Bishop;
+export const king = PieceName.King;
+export const queen = PieceName.Queen;
