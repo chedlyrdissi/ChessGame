2e414d7c204e5f90d45f95cba0b1906d428d1273
Chedlyrdissi <chedlyrd2000@gmail.com>
2020-09-10 22:17:49 -0400

    player switch added board controller finalized

--- src/app/components/board/board-piece.controller.ts
+++ src/app/components/board/board-piece.controller.ts
@@ -1,15 +1,20 @@
-import { PieceName } from './cell/pieces/pieces-names.enum';
+import { PieceName, pawn, rook, queen, king, knight, bishop } from './cell/pieces/pieces-names.enum';
 import { BishopController } from './cell/pieces/bishop/bishop.controller';
 import { RookController } from './cell/pieces/rook/rook.controller';
 import { QueenController } from './cell/pieces/queen/queen.controller';
 import { KingController } from './cell/pieces/king/king.controller';
+import { KnightController } from './cell/pieces/knight/knight.controller';
+import { PawnController } from './cell/pieces/pawn/pawn.controller';
 
 export const ControllerMap = {
-  'bishop': BishopController,
-  'rook': RookController,
-  'queen': QueenController,
-  'king': KingController
+  rook: RookController,
+  king: KingController,
+  pawn: PawnController,
+  queen: QueenController,
+  bishop: BishopController,
+  knight: KnightController
 }
+
 /*
 export const ControllerMap = {
   PieceName.Pawn = 'pawn',
--- src/app/components/board/board.service.ts
+++ src/app/components/board/board.service.ts
@@ -1,5 +1,5 @@
 import { PieceColor } from './cell/pieces/piece-color.enum';
-import { PieceName } from './cell/pieces/pieces-names.enum';
+import { PieceName, pawn, knight, rook, bishop, king, queen } from './cell/pieces/pieces-names.enum';
 import { BishopController } from './cell/pieces/bishop/bishop.controller';
 import { ControllerMap } from './board-piece.controller';
 
@@ -7,11 +7,13 @@ export class BoardService {
 
   gameBoard = START_TABLE;
   possibleSteps: {row: number, column: number}[] = [];
-
+  currentPlayer: PieceColor = PieceColor.White;
   selected: BoardCell = null;
 
   cellClicked(row: number, column: number): void {
+    console.log(this.gameBoard[row][column].f);
     if( this.selected && this.gameBoard[row][column] === this.selected) {
+      // selecting same piece => deselect
       this.selected = null;
       this.possibleSteps = [];
     } else if( this.selected
@@ -21,11 +23,12 @@ export class BoardService {
       if(this.possibleSteps.filter((elem) => {return elem.row === row && elem.column === column}).length > 0) {
         this.gameBoard[row][column] = {c: this.selected.c,p: this.selected.p};
         this.gameBoard[this.selected.row][this.selected.col] = {};
+        this.switchPlayer();
       }
       // deselect
       this.selected = null;
       this.possibleSteps = [];
-    } else if (this.gameBoard[row][column].p) {
+    } else if (this.gameBoard[row][column].p && this.gameBoard[row][column].c === this.currentPlayer) {
       // set selected piece
       this.selected = this.gameBoard[row][column];
       this.selected.row = row;
@@ -38,32 +41,35 @@ export class BoardService {
     console.log('setting steps');
     this.possibleSteps = possibleSteps;
   }
+
+  private switchPlayer(): void {
+    if(this.currentPlayer === PieceColor.White) {
+      this.currentPlayer = PieceColor.Black;
+    } else if(this.currentPlayer === PieceColor.Black) {
+      this.currentPlayer = PieceColor.White;
+    }
+  }
 }
 
 const b = PieceColor.Black;
 const w = PieceColor.White;
-const pawn = PieceName.Pawn;
-const knight = PieceName.Knight;
-const rook = PieceName.Rook;
-const bishop = PieceName.Bishop;
-const king = PieceName.King;
-const queen = PieceName.Queen;
 
 class BoardCell {
   c?: PieceColor;
   p?: PieceName;
   row?: number;
   col?: number;
+  f?: boolean; // first move
 }
 
 const START_TABLE: BoardCell[][] = [
   [{c:b,p:rook},{c:b,p:knight},{c:b,p:bishop},{c:b,p:queen},{c:b,p:king},{c:b,p:bishop},{c:b,p:knight},{c:b,p:rook}],
-  [{c:b,p:pawn},{c:b,p:pawn},{c:b,p:pawn},{c:b,p:pawn},{c:b,p:pawn},{c:b,p:pawn},{c:b,p:pawn},{c:b,p:pawn}],
+  [{c:b,p:pawn,f:true},{c:b,p:pawn,f:true},{c:b,p:pawn,f:true},{c:b,p:pawn,f:true},{c:b,p:pawn,f:true},{c:b,p:pawn,f:true},{c:b,p:pawn,f:true},{c:b,p:pawn,f:true}],
   [{}, {}, {}, {}, {}, {}, {}, {}],
   [{}, {}, {}, {}, {}, {}, {}, {}],
   [{}, {}, {}, {}, {}, {}, {}, {}],
   [{}, {}, {}, {}, {}, {}, {}, {}],
-  [{},{},{c:w,p:pawn},{},{c:w,p:pawn},{c:w,p:pawn},{c:w,p:pawn},{c:w,p:pawn}],
+  [{c:w,p:pawn,f:true},{c:w,p:pawn,f:true},{c:w,p:pawn,f:true},{c:w,p:pawn,f:true},{c:w,p:pawn,f:true},{c:w,p:pawn,f:true},{c:w,p:pawn,f:true},{c:w,p:pawn,f:true}],
   [{c:w,p:rook},{c:w,p:knight},{c:w,p:bishop},{c:w,p:queen},{c:w,p:king},{c:w,p:bishop},{c:w,p:knight},{c:w,p:rook}]
 ];
 
