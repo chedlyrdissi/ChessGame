ab3b8ed39f79ea145e74ed06c155589902c27fa1
Chedlyrdissi <chedlyrd2000@gmail.com>
2020-10-30 22:04:23 -0400

    can create a game api. status: to refine.

--- API/flaskr/active_games.py
+++ API/flaskr/active_games.py
@@ -17,12 +17,46 @@ getActiveGamesQuery = """SELECT ac.id,
                 LEFT JOIN user AS bu ON bu.id = ac.player_black_id
                 LEFT JOIN user AS cu ON cu.id = ac.current_player"""
 
+createGameFullPlayers = """INSERT INTO active_game (player_white_id, player_black_id, current_player) VALUES (?,?,?);"""
+createGamePartialPlayers = """INSERT INTO active_game (player_white_id, current_player) VALUES (?,?);"""
+initiateGamePieces = """INSERT INTO piece_positions (game_id, row, col, piece, color) 
+                            VALUES  (?,0,0,'rook', 'b'),(?,0,7,'rook', 'b'),
+                                    (?,0,1,'knight', 'b'),(?,0,6,'knight', 'b'),
+                                    (?,0,2,'bishop', 'b'),(?,0,5,'bishop', 'b'),
+                                    (?,0,3,'queen', 'b'),(?,0,4,'king', 'b'),
+                                    (?,1,0,'pawn', 'b'),(?,1,1,'pawn', 'b'),
+                                    (?,1,2,'pawn', 'b'),(?,1,3,'pawn', 'b'),
+                                    (?,1,4,'pawn', 'b'),(?,1,5,'pawn', 'b'),
+                                    (?,1,6,'pawn', 'b'),(?,1,7,'pawn', 'b'),
+                                    (?,7,0,'rook', 'w'),(?,7,7,'rook', 'w'),
+                                    (?,7,1,'knight', 'w'),(?,7,6,'knight', 'w'),
+                                    (?,7,2,'bishop', 'w'),(?,7,5,'bishop', 'w'),
+                                    (?,7,3,'queen', 'w'),(?,7,4,'king', 'w'),
+                                    (?,6,0,'pawn', 'w'),(?,6,1,'pawn', 'w'),
+                                    (?,6,2,'pawn', 'w'),(?,6,3,'pawn', 'w'),
+                                    (?,6,4,'pawn', 'w'),(?,6,5,'pawn', 'w'),
+                                    (?,6,6,'pawn', 'w'),(?,6,7,'pawn', 'w');"""
+
 @bp.route('/active-games', methods=('GET', 'POST', 'PUT'))
 def activeGames():
     print("active games")
     if request.method == 'POST':
+        body = eval(request.data)
+
         # create game
-        return jsonify(username='some username', pw='some pw')
+        db = db_get()
+        if('blackPlayer' in body):
+            cursor = db.execute(createGameFullPlayers, (body['whitePlayer'], body['blackPlayer'], body['whitePlayer']))
+        else:
+            cursor = db.execute(createGamePartialPlayers, (body['whitePlayer'], body['whitePlayer']))
+        gameId = cursor.lastrowid
+        print(gameId)
+        db.execute(initiateGamePieces, tuple([gameId for i in range(32)]))
+
+        db.commit()
+        db.close()
+
+        return {'new_game_id': gameId, 'status': 'incomplete'}
     elif request.method == 'GET':
         resp = []
         for row in db_get().execute(getActiveGamesQuery).fetchall():
